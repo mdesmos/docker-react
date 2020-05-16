0bac2b7380349c7efa3589b0591a8977da1ce0c0
desmos <mdesmos@gmail.com>
2020-05-16 17:59:35 +0300

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,10 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test2('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
