638999937929846fe4704cc766eb5016606c8981
desmos <mdesmos@gmail.com>
2020-05-16 21:39:26 +0300

    change app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Произошли изменения
+          Show must go on
         </p>
         <a
           className="App-link"
